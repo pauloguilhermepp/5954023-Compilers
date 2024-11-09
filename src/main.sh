09674c4826a7919e365268d977991697c18174e8
Paulo Guilherme <pauloguilherme@usp.br>
2024-11-09 20:52:35 +0200

    Update main.sh file to properly execute lex/yacc projects

--- src/main.sh
+++ src/main.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-  echo "Error: No file name provided."
-  echo "Usage: $0 <file_name>"
+if [ -z "$2" ]; then
+  echo "Error: Input does not follow standards."
+  echo "Usage: $0 <project_path> <input_file_path>"
   exit 1
 fi
 
-FILE=$1
+PROJECT_PATH=$1
+INPUT_FILE_PATH=$2
 
-lex -o ${FILE}.yy.c ${FILE}.l
-bison -d -o ${FILE}.tab.c ${FILE}.y
-gcc ${FILE}.yy.c ${FILE}.tab.c -o ${FILE} -ll
+lex -o ${PROJECT_PATH}.yy.c ${PROJECT_PATH}.l
+bison -d -o ${PROJECT_PATH}.tab.c ${PROJECT_PATH}.y
+gcc -o ${PROJECT_PATH} ${PROJECT_PATH}.tab.c
 
-./${FILE}
+./${PROJECT_PATH} ${INPUT_FILE_PATH}
